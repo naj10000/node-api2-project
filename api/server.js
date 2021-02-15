fad8832d718eda983d3a1505dfb0f3b866668552
NaJ <naj10000@live.com>
2021-02-15 16:56:17 -0500

    setup

--- api/server.js
+++ api/server.js
@@ -1,2 +1,17 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require("express")
+
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.json({
+        message: 'WELCOME'
+    })
+})
+
+
+module.exports = server
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it here
+const server = require("./api/server.js")
+
+const PORT = 5000
+
+server.listen(PORT, () => {
+    console.log(`Server is Running on http://localhost:${PORT}`)
+})
